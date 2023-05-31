812bcb3ef83089a1e42e4703adc844e391ce6744
ikromjon008 <129050303+ikromjon008@users.noreply.github.com>
2023-05-31 12:10:26 +0500

    Update botcham.py

--- botcham.py
+++ botcham.py
@@ -1,1 +1,16 @@
+import telebot
+
+
+Token = "5773060087:AAHpLmsAqn4b8eIQaL997SX85N7wRE5R2WQ"
+
+
+bot = telebot.TeleBot(Token)
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id,"salom")
+
+
+bot.polling(none_stop=True)
 
